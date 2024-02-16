e330080f329873038c2f19a5ef3939f87178c450
pp_o <127409900+ppo-0@users.noreply.github.com>
2024-02-16 17:45:25 +0800

    feat:新增写作帖子时的预览功能 (#61)

--- src/views/posts/write.vue
+++ src/views/posts/write.vue
@@ -15,11 +15,12 @@
           <input 
             type="text" 
             placeholder="请输入标题(最多50字)"
+            v-model="postTitle"
           >
         </div>
         <Editor
           class="editor"
-          style="height: 500px; overflow-y: hidden;"
+          style="min-height: 700px; overflow-y: hidden;"
           v-model="valueHtml"
           :defaultConfig="editorConfig"
           :mode="mode"
@@ -45,7 +46,7 @@
             <i class="iconfont icon-jia"></i>
           </div>
           <div class="operate">
-            <button>预览</button>
+            <button @click="previewPost">预览</button>
             <button>发布</button>
           </div>
         </div>
@@ -57,7 +58,7 @@
 <script>
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
+import { onBeforeUnmount, ref, shallowRef } from 'vue'
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 
 export default {
@@ -66,8 +67,43 @@ export default {
     // 编辑器实例，必须用 shallowRef
     const editorRef = shallowRef()
 
-    const toolbarConfig = {}
+    const toolbarConfig = {
+      toolbarKeys: [
+        // 一些常用的菜单 key
+        'undo', // 撤销
+        'redo', // 重做
+        'clearStyle', // 清除格式
+        '|',
+        'headerSelect', // 标题
+        'blockquote', // 引用
+        "codeBlock", // 代码块
+        'bold', // 加粗
+        'italic', // 斜体
+        'through', // 删除线
+        'underline', // 下划线
+        'divider', // 分割线
+        'bulletedList', // 无序列表
+        'numberedList', // 有序列表
+        'color', // 文字颜色
+        "bgColor", // 背景颜色
+        'insertLink', // 插入链接
+        'fontSize', // 字体大小
+        "fontFamily",
+        'lineHeight', // 行高
+        'uploadImage', // 上传图片
+        'uploadVideo',//上传视频
+        'delIndent', // 缩进
+        'indent', // 增进
+        'deleteImage',//删除图片
+        'insertTable', // 插入表格
+        'justifyCenter', // 居中对齐
+        'justifyJustify', // 两端对齐
+        'justifyLeft', // 左对齐
+        'justifyRight', // 右对齐
+      ]
+    }
     const editorConfig = { placeholder: '请输入内容...' }
+    const postTitle = ref('') // 文章标题
 
     // 组件销毁时，也及时销毁编辑器
     onBeforeUnmount(() => {
@@ -84,13 +120,22 @@ export default {
       console.log(editorRef.value.getHtml()) // 获取内容
     }
 
+    const previewPost = () => {
+      sessionStorage.setItem('postTitle', postTitle.value)
+      sessionStorage.setItem('postContent', editorRef.value.getHtml())
+      console.log(editorRef.value.getHtml())
+      window.open('/write/preview')
+    }
+
     return {
       editorRef,
       mode: 'default', // 或 'simple'
       toolbarConfig,
       editorConfig,
+      postTitle,
       handleCreated,
-      clickFabu
+      clickFabu,
+      previewPost
     }; 
   }
 }
@@ -99,9 +144,8 @@ export default {
 <style scoped lang="css">
 .main-box {
   width: 100%;
-  height: 100%;
   background-color: #fff;
-  overflow-y: hidden;
+  overflow-y: auto;
   display: flex;
 
   .editor-box {
@@ -112,17 +156,23 @@ export default {
     background-color: #f5f5f5;
     border-radius: 5px;
     border: 1px solid #ccc;
-    overflow-y: auto;
 
     .header {
+      position: fixed;
       width: 100%;
+      top: 0px;
       background-color: #fff;
       box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
+      margin: 0 auto;
       margin-bottom: 20px;
-      z-index: 10;
+      z-index: 3;
       display: flex;
+      justify-content: center;
 
       .back {
+        position: absolute; 
+        top: 0;
+        left: 10px;
         font-size: 35px;
         margin-left: 10px;
         cursor: pointer;
@@ -131,16 +181,13 @@ export default {
       .back:hover {
         color: #000;
       }
-
-      .toolbar {
-        margin: 0 auto;
-      }
     }
 
     .section {
-      width: 60%;
+      width: 1000px;
       box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
       margin: auto;
+      margin-top: 80px;
       padding: 20px;
       background-color: #fff;
 
@@ -161,16 +208,15 @@ export default {
         }
         input::placeholder {
           font-size: 30px;
-          color: #727272;
+          color: #adadad;
         }
       }
   
       .editor {
         background-color: #fff;
-        overflow-y: hidden;
         border-bottom: 1px solid #ccc;
-        background-color: #833636;
         margin-bottom: 20px;
+        color: #fff;
       }
 
       .footer {
@@ -232,8 +278,8 @@ export default {
           }
 
           button:nth-child(1) {
-            background-color: #ccc;
-            color: #4e4c4c;
+            background-color: #cccccc8b;
+            color: #4e4c4c93;
           }
           button:nth-child(2) {
             background-color: #5383dd;
