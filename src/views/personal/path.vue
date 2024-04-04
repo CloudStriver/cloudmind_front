cfe376d46a99dc0507d080782f06e7868dadb071
ppo-0 <1304578864@qq.com>
2024-04-04 16:17:30 +0800

    feat:新增文件排序样式，还需完善取消弹框功能

--- src/views/personal/path.vue
+++ src/views/personal/path.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="main-box"> 
+    <div class="main-box" @click="cancelShowSort($event)"> 
         <div class="path">
             <div
                 class="path-item"
@@ -20,8 +20,90 @@
                 >全选
             </label>
             <div class="sort">
-                <i class="iconfont icon-paixu"></i>
-                <span>按照时间排序</span>
+                <div @click.stop="clickSort">
+                    <i class="iconfont icon-paixu"></i>
+                    <span>按照时间排序</span>
+                </div>
+                <div class="sort-box" v-show="isShowSort">
+                    <ul>
+                        <li>
+                            <input 
+                                type="radio"
+                                name="typeSort"
+                                id="name"
+                                v-model="typeSort"
+                                value="name"
+                                checked
+                            >
+                            <i v-show="typeSort === 'name'" class="iconfont icon-0-58"></i>
+                            <i v-show="typeSort !== 'name'" class="iconfont icon-0-58 i"></i>
+                            <label for="name">名称</label>
+                        </li>
+                        <li>
+                            <input 
+                                type="radio"
+                                name="typeSort"
+                                id="createTime"
+                                v-model="typeSort"
+                                value="createTime"
+                            >
+                            <i v-show="typeSort === 'createTime'" class="iconfont icon-0-58"></i>
+                            <i v-show="typeSort !== 'createTime'" class="iconfont icon-0-58 i"></i>
+                            <label for="createTime">创建时间</label>
+                        </li>
+                        <li>
+                            <input 
+                                type="radio"
+                                name="typeSort"
+                                id="updateTime"
+                                v-model="typeSort"
+                                value="updateTime"
+                            >
+                            <i v-show="typeSort === 'updateTime'" class="iconfont icon-0-58" ></i>
+                            <i v-show="typeSort !== 'updateTime'" class="iconfont icon-0-58 i" ></i>
+                            <label for="updateTime">修改时间</label>
+                        </li>
+                        <li>
+                            <input 
+                                type="radio"
+                                name="typeSort"
+                                id="size"
+                                v-model="typeSort"
+                                value="size"
+                            >
+                            <i v-show="typeSort === 'size'" class="iconfont icon-0-58"></i>
+                            <i v-show="typeSort !== 'size'" class="iconfont icon-0-58 i"></i>
+                            <label for="size">文件大小</label>
+                        </li>
+                    </ul>
+                    <div class="line"></div>
+                    <ul>
+                        <li>
+                            <input 
+                                type="radio"
+                                name="sort"
+                                id="asc"
+                                v-model="sort"
+                                value="asc"
+                            >
+                            <i v-show="sort === 'asc'" class="iconfont icon-0-58"></i>
+                            <i v-show="sort !== 'asc'" class="iconfont icon-0-58 i"></i>
+                            <label for="asc">升序</label>
+                        </li>
+                        <li>
+                            <input 
+                                type="radio"
+                                name="sort"
+                                id="desc"
+                                v-model="sort"
+                                value="desc"
+                            >
+                            <i  v-show="sort === 'desc'" class="iconfont icon-0-58"></i>
+                            <i  v-show="sort !== 'desc'" class="iconfont icon-0-58 i"></i>
+                            <label for="desc">降序</label>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -34,7 +116,10 @@ import router from '@/router'
 
 const store = useStore()
 const emit = defineEmits(['sendPathMsg'])
+const typeSort = ref("name")
+const sort = ref("asc")
 const allSelect = ref(false)
+const isShowSort = ref(false)
 const pathData = ref({
     pathId: [] as string[],
     pathName: [] as string[]
@@ -74,6 +159,18 @@ watch(() => allSelect.value, (newVal) => {
     })
 })
 
+const cancelShowSort = (e: MouseEvent) => {
+    //还需要再完善取消弹框的方法
+    document.querySelectorAll('.sort-box').forEach((item) => {
+        if (!item.contains(e.target as Node)) {
+            isShowSort.value = false
+        }
+    })
+}
+
+const clickSort = () => {
+    isShowSort.value = !isShowSort.value
+}
 const toPath = (index: number) => {
     router.push({name: 'personal', params: {fatherId: pathData.value.pathId[index]}})
 }
@@ -138,6 +235,7 @@ const toPath = (index: number) => {
         }
 
         .sort {
+            position: relative;
             font-size: 15px;
             color: #646363;
             cursor: pointer;
@@ -145,6 +243,60 @@ const toPath = (index: number) => {
 
             i {
                 margin-right: 5px;
+            }
+
+            .sort-box {
+                position: absolute;
+                width: 130px;
+                top: 30px;
+                left: -10px;
+                border-radius: 10px;
+                background-color: #fff;
+                box-shadow: 0 0 5px #cecece;
+                padding: 10px;
+
+                .line {
+                    width: 100%;
+                    height: 1px;
+                    background-color: #e5e5e56e;
+                    margin-top: 10px;
+                    margin-bottom: 10px;
+                }
+
+                ul {
+                    list-style: none;
+                    margin: 0;
+                    padding: 0;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+
+                    li {
+                        margin: 5px 0;
+                        display: flex;
+                        align-items: center;
+                        margin-right: 10px;
+                        color: #789bbc;
+
+                        input {
+                            display: none;
+                        }
+
+                        label {
+                            width: 100%;
+                            font-size: 15px;
+                            cursor: pointer;
+                            user-select: none;
+                        }
+                        label:hover {
+                            color: #789bbc;
+                        }
+
+                        .i {
+                            color: #fff;
+                        }
+                    }
+                }
             }
         }
     }
